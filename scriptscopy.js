b3a69245e28cb7145f65fdbc011ac6bf03d40353
joshgaspar01 <46583175+joshgaspar01@users.noreply.github.com>
2019-10-09 19:57:22 -0400

    Add files via upload

---
+++ scriptscopy.js
@@ -0,0 +1,25 @@
+scrollVert();
+var scrollLeft=0;
+
+
+function scrollVert() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollTop -= (delta * 1);
+    e.preventDefault();
+    setTimeout(function() {
+      if ($(window).scrollTop() + $(window).height() == $(document).height())
+        scrollHoriz();
+    }, 0)
+
+  });
+}
+function scrollHoriz() {
+  $('html, body, *').off('mousewheel').mousewheel(function(e, delta) {
+    this.scrollLeft -= (delta * 1);
+    e.preventDefault();
+    scrollLeft=this.scrollLeft
+    setTimeout(function() {
+      if (scrollLeft == 0) scrollVert();
+    }, 0)
+  });
+}
